c430a5df378bea9da046bcad8005ef8cc40c0935
Denisalik <schegletovdenis@mail.ru>
2021-09-13 03:07:23 +0300

    added template for questions

--- components/Question/Question.tsx
+++ components/Question/Question.tsx
@@ -1,5 +1,18 @@
 import React from "react";
-import {Card, Grid} from "@material-ui/core";
+import {
+    Card,
+    CardContent,
+    Grid,
+    Button,
+    CardActions,
+    TextField,
+    MenuItem,
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    FormControlLabel,
+    Radio
+} from "@material-ui/core";
 import classes from "./Question.module.scss";
 
 const Question:React.FC = () => {
@@ -7,7 +20,34 @@ const Question:React.FC = () => {
     return (
         <Grid className={classes.Question}>
             <Card>
+                <CardContent>
+                    <FormControl component="fieldset">
+                        <FormLabel component="legend">Gender</FormLabel>
+                        <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                            <FormControlLabel value="female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="male" control={<Radio />} label="Male" />
+                            <FormControlLabel value="other" control={<Radio />} label="Other" />
+                        </RadioGroup>
+                    </FormControl>
+                    <form noValidate autoComplete="off">
+                        <TextField id="standard-basic"/>
+                        <TextField
+                            error={false}
+                            id="standard-select-option-1"
+                            select
+                            label="select-1"
+                            value={null}
+                            onChange={()=>()}
+                            helperText="Please select your option"
+                        >
+                            <MenuItem key={} value={}></MenuItem>
+                        </TextField>
+                    </form>
 
+                </CardContent>
+                <CardActions>
+                    <Button></Button>
+                </CardActions>
             </Card>
         </Grid>
     );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type {AppProps} from "next/app"
 import Layout from "../components/Layout";
 import React from "react";
 import {SwitchTransition, CSSTransition} from "react-transition-group";
+import "../styles/Router.module.scss";
 
 function MyApp({Component, pageProps, router}: AppProps) {
     //todo check if animation works
