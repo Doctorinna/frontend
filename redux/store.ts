04ca07f99ea8c40ff6f2c0c719d55380b7be9efa
Denisalik <schegletovdenis@mail.ru>
2021-09-14 10:02:44 +0300

    added redux dev tools

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,10 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 import {rootReducer} from "./reducers";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+let composeEnhancers = compose;
+
+if(typeof window !== "undefined")
+    composeEnhancers = (window as any)["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] as typeof compose || compose;
+
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
