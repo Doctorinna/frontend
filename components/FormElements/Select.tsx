1b04b45faaf273d70b0440d91ff782fdfeac87d8
Denisalik <schegletovdenis@mail.ru>
2021-10-06 08:16:38 +0300

    resolved material-ui import errors due to versions

--- components/FormElements/Select.tsx
+++ components/FormElements/Select.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {TextField} from "@material-ui/core";
-import { MenuItem } from "material-ui";
+import { MenuItem, TextField } from "@mui/material";
 
 interface SelectProps {
     options: string[],
