32e591642ccf9e58917c8d36f309794af1fe1531
Denisalik <schegletovdenis@mail.ru>
2021-10-06 08:20:06 +0300

    resolved material-ui import errors due to versions

--- components/Question/Results.tsx
+++ components/Question/Results.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Grid, Typography} from "@material-ui/core";
+import {Grid, Typography} from "@mui/material";
 
 const Results: React.FC = () => {
     const risks = ["a", "b", "c", "d"];
