b4f149b3c620f7dc0021df25ffd58c9abb430f72
Denisalik <schegletovdenis@mail.ru>
2021-10-10 23:57:19 +0300

    design enhancement

--- __tests__/root.spec.tsx
+++ __tests__/root.spec.tsx
@@ -1,11 +1,12 @@
 import 'jsdom-global/register';
 import React from "react";
 import {ReactWrapper, shallow, mount, ShallowWrapper} from "enzyme";
-import {Grid} from "@mui/material";
+import {Box, Grid} from "@mui/material";
 import Index from "../pages/index";
 import QuestionnaireHoc from "../components/hoc/QuestionnaireHOC";
 import {Provider} from "react-redux";
 import {makeStore} from "../redux/store";
+import Layout from "../components/Layout/Layout";
 jest.useFakeTimers()
 
 describe("Pages", ()=>{
@@ -16,6 +17,11 @@ describe("Pages", ()=>{
             </Provider>);
             const grid_number = wrapper.find(Grid).length;
             expect(grid_number).toEqual(16);
+        });
+        it("Layout is rendered", ()=>{
+            const wrapper: ShallowWrapper = shallow(<Layout/>);
+            const box_number = wrapper.find(Box).length;
+            expect(box_number).toEqual(2);
         })
         it("All Grid components in questionnaire HOC are rendered", ()=>{
             const wrapper: ReactWrapper = mount(<QuestionnaireHoc underline={2}/>);
